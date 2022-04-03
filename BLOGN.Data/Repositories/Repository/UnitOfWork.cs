03925603c54990ff0311dc26ed085056aebd5082
Furkan Kuş <furkankus@gmail.com>
2022-04-03 15:15:22 +0300

    UnitOfWork ve Category oluşturuldu.

---
+++ BLOGN.Data/Repositories/Repository/UnitOfWork.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BLOGN.Data.Repositories.IRepositories;
+
+namespace BLOGN.Data.Repositories.Repository
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly ApplicationDbContext _context;
+
+        public UnitOfWork(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public ICategoryRepository Category => new CategoryRepository(_context);
+
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
+
+        public void Save()
+        {
+            _context.SaveChanges();
+        }
+
+        public async Task SaveAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+    }
+}
